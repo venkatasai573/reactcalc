529f4f5f6d814fa971a48469ab572059c7b274e3
saichinnu <venkatasai.573@gmail.com>
2019-01-30 12:11:19 +0530

    Updates

---
+++ precache-manifest.c8b2905e9c70e689b40860edb3f41f03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72b6656c780023c1d612",
+    "url": "/reactcalc/static/js/runtime~main.72b6656c.js"
+  },
+  {
+    "revision": "86971e11491ca20e5601",
+    "url": "/reactcalc/static/js/main.86971e11.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/reactcalc/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "86971e11491ca20e5601",
+    "url": "/reactcalc/static/css/main.29dd5dee.chunk.css"
+  },
+  {
+    "revision": "fdbf7e3bc4f875674a307962adede081",
+    "url": "/reactcalc/index.html"
+  }
+];
